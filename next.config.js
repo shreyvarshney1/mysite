41f6566f32c3ade07b8f8ea5940bbcad733785f6
Shrey5908 <112169483+Shrey5908@users.noreply.github.com>
2024-03-09 21:05:31 +0530

    Update next.config.js to change output to 'export'

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
