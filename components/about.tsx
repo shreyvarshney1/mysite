c12cd11caefc93e3cf3c644d5bd87c0e789f6796
Shrey5908 <112169483+Shrey5908@users.noreply.github.com>
2024-03-09 20:27:27 +0530

    initial commit

---
+++ components/about.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        A passionate{" "}
+        <span className="font-medium">B.Tech</span>{" "}student, diving headfirst
+        into the world of code. With{" "}
+        <span className="font-medium">
+          Python, C++, and Web Development skills
+        </span>{" "}
+        in my toolbox,{" "}
+        <span className="italic">
+          I'm on a mission to become a senior developer at a
+        </span>{" "}
+        <span className="underline">top-tier product company</span>. My Web
+        Development skills include{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and exploring{" "}
+        <span className="italic">Geeky Stuff</span>. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">
+          Geopolitics and Artificial Intelligence
+        </span>
+        . I'm also learning how to play the guitar.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,107 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap, LuSchool2 } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Schooling",
+    location: "Delhi, India",
+    description:
+      "I passed by 10th and 12th from M.L. Khanna D.A.V. Public School, Dwarka. Here, I learned the basics of programming and web development. I also learned about the importance of teamwork and leadership.",
+    icon: React.createElement(LuSchool2),
+    date: "2019-2022",
+  },
+  {
+    title: "Graduation",
+    location: "Bhopal, India",
+    description:
+      "I graduated with a B.Tech in Computer Science from Vellore Institute of Technology, Bhopal. I learned about data structures, algorithms, and software engineering. I also learned about teamwork and leadership. I was also the head of the college's coding club.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022 - 2026",
+  },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "Houston, TX",
+  //   description:
+  //     "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2021 - present",
+  // },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "C++",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Java",
+  "Framer Motion",
+] as const;
